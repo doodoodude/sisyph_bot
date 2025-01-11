100888178ce2c9b28e684c023a0fc022e43f7d94
doodoodude <pay-hoy@yandex.ru>
2025-01-11 16:56:11 +0300

    - robot tf pub tests

--- sisyph_slam/scripts/robot_tf_pub.py
+++ sisyph_slam/scripts/robot_tf_pub.py
@@ -20,12 +20,12 @@ tf_msg1.child_frame_id = "usb_cam"
 tf_msg1.transform.rotation = Quaternion(*ident_quat)
 
 tf_msg2 = TransformStamped()
-tf_msg2.header.frame_id = "odom" 
+tf_msg2.header.frame_id = "world" 
 tf_msg2.child_frame_id = "robot" 
 tf_msg2.transform.rotation = Quaternion(*ident_quat)
 
 tf_robot_laser_msg = TransformStamped()
-tf_robot_laser_msg.header.frame_id = "robot" 
+tf_robot_laser_msg.header.frame_id = "world" 
 tf_robot_laser_msg.child_frame_id = "laser"  
 tf_robot_laser_msg.transform.rotation = Quaternion(*flipz_quat)
 
@@ -68,32 +68,7 @@ def fiducial_transforms_cb(fid_msg: FiducialTransformArray):
         #     tf_msg2.transform = fid_tf.transform
         #     tf_broadcaster.sendTransform(tf_msg2)
 
-        # if fid_tf.fiducial_id == 46:   # world -> robot
-        #     tf_quat2 = get_quat_arr_from_tf_msg(fid_tf)
-        #     tf_trans2 = get_trans_arr_from_tf_msg(fid_tf)
-
-        #     tf_quat1_inv = get_quat_arr_from_tf_msg(tf_msg1)
-        #     tf_trans1_inv = get_trans_arr_from_tf_msg(tf_msg1)
-
-        #     tf_quat2 = quaternion_multiply(tf_quat1_inv, tf_quat2)
-        #     tf_trans2 = rotate_vector_by_quat(tf_trans2, tf_quat1_inv) + tf_trans1_inv
-
-        #     tf_msg2.transform.rotation = Quaternion(*tf_quat2)
-        #     tf_msg2.transform.translation.x = tf_trans2[0]
-        #     tf_msg2.transform.translation.y = tf_trans2[1]
-        #     tf_msg2.transform.translation.z = tf_trans2[2]            
-        #     tf_broadcaster.sendTransform(tf_msg2)
-
-        if fid_tf.fiducial_id == 46: # odom -> usb_cam
-            tf_quat1_inv = quaternion_inverse(tf_quat) #quaternion_multiply(correct_fid_quat, tf_quat) 
-            tf_trans1_inv = -rotate_vector_by_quat(tf_trans, tf_quat1_inv) 
-
-            tf_msg1.transform.rotation = Quaternion(*tf_quat1_inv)
-            tf_msg1.transform.translation.x = tf_trans1_inv[0]
-            tf_msg1.transform.translation.y = tf_trans1_inv[1]
-            tf_msg1.transform.translation.z = tf_trans1_inv[2]
-
-        if fid_tf.fiducial_id == 42:   # odom -> robot
+        if fid_tf.fiducial_id == 46:   # world -> robot
             tf_quat2 = get_quat_arr_from_tf_msg(fid_tf)
             tf_trans2 = get_trans_arr_from_tf_msg(fid_tf)
 
@@ -109,7 +84,33 @@ def fiducial_transforms_cb(fid_msg: FiducialTransformArray):
             tf_msg2.transform.translation.z = tf_trans2[2]            
             tf_broadcaster.sendTransform(tf_msg2)
 
-    tf_broadcaster.sendTransform(tf_robot_laser_msg)
+        # if fid_tf.fiducial_id == 46: # odom -> usb_cam
+        #     tf_quat1_inv = quaternion_inverse(tf_quat) #quaternion_multiply(correct_fid_quat, tf_quat) 
+        #     tf_trans1_inv = -rotate_vector_by_quat(tf_trans, tf_quat1_inv) 
+
+        #     tf_msg1.transform.rotation = Quaternion(*tf_quat1_inv)
+        #     tf_msg1.transform.translation.x = tf_trans1_inv[0]
+        #     tf_msg1.transform.translation.y = tf_trans1_inv[1]
+        #     tf_msg1.transform.translation.z = tf_trans1_inv[2]
+
+        # if fid_tf.fiducial_id == 42:   # odom -> robot
+        #     tf_quat2 = get_quat_arr_from_tf_msg(fid_tf)
+        #     tf_trans2 = get_trans_arr_from_tf_msg(fid_tf)
+
+        #     tf_quat1_inv = get_quat_arr_from_tf_msg(tf_msg1)
+        #     tf_trans1_inv = get_trans_arr_from_tf_msg(tf_msg1)
+
+        #     tf_quat2 = quaternion_multiply(tf_quat1_inv, tf_quat2)
+        #     tf_trans2 = rotate_vector_by_quat(tf_trans2, tf_quat1_inv) + tf_trans1_inv
+
+        #     tf_msg2.transform.rotation = Quaternion(*tf_quat2)
+        #     tf_msg2.transform.translation.x = tf_trans2[0]
+        #     tf_msg2.transform.translation.y = tf_trans2[1]
+        #     tf_msg2.transform.translation.z = tf_trans2[2]            
+        #     tf_broadcaster.sendTransform(tf_msg2)
+
+    # tf_quat2 = quaternion_multiply(tf_quat2, flipz_quat)
+    tf_broadcaster.sendTransform(tf_msg2)
 
 # Subscribe to the /fiducial_transforms topic
 fid_listener = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, fiducial_transforms_cb)
