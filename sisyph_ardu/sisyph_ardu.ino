55257e445b660e61a29478df52b43a84568f2c0d
doodoodude <pay-hoy@yandex.ru>
2025-04-17 19:29:08 +0300

    - sisyph ardu: added cmd timeout

--- sisyph_ardu/sisyph_ardu.ino
+++ sisyph_ardu/sisyph_ardu.ino
@@ -13,63 +13,78 @@ int L_EN[motorCount] = {22, 26, 30, 34};
 int LPWM[motorCount] = {2, 4, 6, 8};
 int cmd_sign[motorCount] = {-1, 1, 1, -1};
 
+unsigned long cmd_timeout_ms = 3000;
+unsigned long cmd_last_time = 0.0;
+
 
 void commandCallback(const std_msgs::Int8MultiArray& cmd_msg) {
-  if(!cmd_msg.data[4]) {
-    for (int i = 0; i < motorCount; i++) {
-      setMotorSpeed(RPWM[i], LPWM[i], cmd_sign[i]*cmd_msg.data[i]); 
-    }
-  }
-  else
-  {
-    for (int i = 0; i < motorCount; i++) {
-      setMotorSpeed(RPWM[i], LPWM[i], 0); 
-    }
-  }
+
+	cmd_last_time = millis();	
+
+	if(!cmd_msg.data[4]) {
+		for (int i = 0; i < motorCount; i++) {
+			setMotorSpeed(RPWM[i], LPWM[i], cmd_sign[i]*cmd_msg.data[i]); 
+		}
+	}
+	else	
+	{
+		for (int i = 0; i < motorCount; i++) {
+			setMotorSpeed(RPWM[i], LPWM[i], 0); 
+		}
+	}
 
 }
 
 ros::Subscriber<std_msgs::Int8MultiArray> sub("/sisyph/wheel_cmd", commandCallback);
 
 void setup() {
-  nh.initNode();
-  nh.subscribe(sub);
+	nh.initNode();
+	nh.subscribe(sub);
 
-  for (int i = 0; i < motorCount; i++) {
-    pinMode(R_EN[i], OUTPUT); // настраиваем пины
-    pinMode(RPWM[i], OUTPUT);
-    pinMode(L_EN[i], OUTPUT);
-    pinMode(LPWM[i], OUTPUT);
-    digitalWrite(R_EN[i], HIGH);// "включаем" пин, для управления вращением "вперёд"
-    digitalWrite(L_EN[i], HIGH);// "включаем" пин, для управления вращением "назад"
-  }
+	for (int i = 0; i < motorCount; i++) {
+		pinMode(R_EN[i], OUTPUT); // настраиваем пины
+		pinMode(RPWM[i], OUTPUT);
+		pinMode(L_EN[i], OUTPUT);
+		pinMode(LPWM[i], OUTPUT);
+		digitalWrite(R_EN[i], HIGH);// "включаем" пин, для управления вращением "вперёд"
+		digitalWrite(L_EN[i], HIGH);// "включаем" пин, для управления вращением "назад"
+	}
 }
 
 
 void loop() {
-  nh.spinOnce();
-  delay(10);
-//  setMotorSpeed(RPWM[3], LPWM[3], 50); 
-//  delay(1000);
-//  setMotorSpeed(RPWM[3], LPWM[3], 0);
-//  delay(500);
-//  setMotorSpeed(RPWM[3], LPWM[3], -50);  
-//  delay(2000);
-//  setMotorSpeed(RPWM[3], LPWM[3], 0);
-//  delay(500);
+	nh.spinOnce();
+
+	if((millis()-cmd_last_time)>cmd_timeout_ms)
+	{
+		for (int i = 0; i < motorCount; i++) {
+			setMotorSpeed(RPWM[i], LPWM[i], 0); 
+		}
+	}
+
+	//  setMotorSpeed(RPWM[3], LPWM[3], 50); 
+	//  delay(1000);
+	//  setMotorSpeed(RPWM[3], LPWM[3], 0);
+	//  delay(500);
+	//  setMotorSpeed(RPWM[3], LPWM[3], -50);  
+	//  delay(2000);
+	//  setMotorSpeed(RPWM[3], LPWM[3], 0);
+	//  delay(500);
 }
 
 
 
 // Функция для установки скорости и направления двигателя
 void setMotorSpeed(int RPWM, int LPWM, int speed) {
-  if (speed >= 0) {
-    // Направление вперед
-    analogWrite(RPWM, speed);
-    analogWrite(LPWM, 0);
-  } else {
-    // Направление назад
-    analogWrite(RPWM, 0);
-    analogWrite(LPWM, -speed);
-  }
+	if (speed >= 0) {
+		// Направление вперед
+		analogWrite(RPWM, speed);
+		analogWrite(LPWM, 0);
+	} 
+	else 
+	{
+		// Направление назад
+		analogWrite(RPWM, 0);
+		analogWrite(LPWM, -speed);
+	}
 }
